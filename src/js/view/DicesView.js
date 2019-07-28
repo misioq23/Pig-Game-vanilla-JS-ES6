9ce5c27816cf65418260f323ad7b9279758dcb1b
misioq23 <mi23chal@gmail.com>
2019-07-28 16:38:39 +0200

    small design improvments

--- src/js/view/DicesView.js
+++ src/js/view/DicesView.js
@@ -12,7 +12,7 @@ const dicesView = {
 	render(numberArr) {
 		this.remove();
 		const view = numberArr.map((number, index) => `
-			<img src="assets/dice-${number}.svg" alt="Dice" class="${strings.dice}" id="${strings.dice}-${index + 1}">
+			<img src="assets/dice-${number}.svg" alt="Dice" class="${strings.dice} ${strings.dice}-${index + 1}">
 		`).join(' ');
 		element.appWrapper.insertAdjacentHTML('afterbegin', view);
 	},
